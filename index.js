5c954b0a05e90e9ed94d3c04651637f1474a0d95
lucas <lucasportellaagu@gmail.com>
2021-08-27 18:49:14 -0300

    add error middlewares

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ const {
 const app = express();
 app.use(bodyParser.json());
 
+app.use((err, req, res, next) => {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
@@ -29,7 +33,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (req, res) => {
+app.get('/talker', rescue(async (req, res) => {
   const talkerData = await getTalkerData();
   if (talkerData.length > 0) {
     return res.status(200).json(talkerData);
@@ -37,23 +41,23 @@ app.get('/talker', async (req, res) => {
   if (talkerData.length === 0) {
     return res.status(200).json([]);
   }
-});
+}));
 
-app.get('/talker/search', talkerPostValidator, async (req, res) => {
+app.get('/talker/search', talkerPostValidator, rescue(async (req, res) => {
   const queryResult = await searchTalkerByQuery(req.query.q);
   return res.status(200).json(queryResult);
-});
+}));
 
-app.get('/talker/:id', async (req, res) => {
+app.get('/talker/:id', rescue(async (req, res) => {
   const searchResult = await findTalkerById(req.params.id);
   if (searchResult) {
     return res.status(200).json(searchResult);
   } 
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-});
+}));
 
-app.post('/login', authMiddleware, async (req, res) =>
-  res.status(200).json({ token: generateRandomToken() }));
+app.post('/login', authMiddleware, rescue(async (req, res) =>
+res.status(200).json({ token: generateRandomToken() })));
 
 app.post(
   '/talker',
@@ -61,10 +65,10 @@ app.post(
   nameAgeValidator,
   talkObjValidator1,
   talkObjValidator2,
-  async (req, res) => {
+  rescue(async (req, res) => {
     const newTalker = await addTalkerData(req.body);
     return res.status(201).json(newTalker);
-  },
+  }),
 );
 
 app.put(
@@ -73,16 +77,16 @@ app.put(
   nameAgeValidator,
   talkObjValidator1,
   talkObjValidator2,
-  async (req, res) => {
-    const updatedTalker = await editTalkerById(req.body, req.params.id);
-    return res.status(200).json(updatedTalker);
-  },
+ rescue(async (req, res) => {
+  const updatedTalker = await editTalkerById(req.body, req.params.id);
+  return res.status(200).json(updatedTalker);
+}),
 );
 
-app.delete('/talker/:id', talkerPostValidator, async (req, res) => {
+app.delete('/talker/:id', talkerPostValidator, rescue(async (req, res) => {
   await deleteTalkerById(req.params.id);
   return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
-});
+}));
 
 app.listen(PORT, () => {
   console.log('Online');
